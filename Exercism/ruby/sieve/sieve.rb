d848dcf6949b038d7ff446c6be259fc7d80f27d5
mb-kaizen <michael.entr@gmail.com>
2018-06-18 02:04:10 +0000

    compelted Sieve and added roman numerals

--- Exercism/ruby/sieve/sieve.rb
+++ Exercism/ruby/sieve/sieve.rb
@@ -1,12 +1,21 @@
 class Sieve
+  attr_reader :primes
 
   def initialize(limit)
-    @limit = limit
-  end
+    @primes = []
 
-  def primes
-    
-  end
+    return if limit < 2
 
+    nums = (2..limit).to_a
+    while nums.size > 0
+      head = nums.first
+      @primes.push head
+      nums = nums.select { |n| n % head != 0 }
+    end
+  end
+  
 end
 
+module BookKeeping
+  VERSION = 1
+end
