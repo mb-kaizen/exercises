d8bc39a61bafff29552af0d73cead989f435c927
mb-kaizen <michael.entr@gmail.com>
2018-06-24 09:31:57 +0000

    roman numerals solution

--- Exercism/ruby/roman-numerals/roman_numerals.rb
+++ Exercism/ruby/roman-numerals/roman_numerals.rb
@@ -1,10 +1,33 @@
- class Roman_numerals
-  
-  def to_roman
-    return "I"
-  end
+class Fixnum
+  MAPPING = [
+    [1000, "M" ],
+    [900,  "CM"],
+    [500,  "D" ],
+    [400,  "CD"],
+    [100,  "C" ],
+    [90,   "XC"],
+    [50,   "L" ],
+    [40,   "XL"],
+    [10,   "X" ],
+    [9,    "IX"],
+    [5,    "V" ],
+    [4,    "IV"],
+    [1,    "I" ]
+  ]
 
+  def to_roman
+    n = self
+    result = ""
+    MAPPING.each do |limit, roman|
+      while n >= limit
+        result << roman
+        n -= limit
+      end
+    end
+    result
+  end
 end
+
 
 module BookKeeping
   VERSION = 2
