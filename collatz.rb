a9c6057b6a095165aea7a375de31cb0ad8444de2
mb-kaizen <michael.entr@gmail.com>
2018-07-11 02:15:26 +0000

    working on collatz

--- collatz.rb
+++ collatz.rb
@@ -1,15 +1,37 @@
 # Given an input of n, the next number is defined by:
 # n is even: n -> n/2
 # n is odd: n -> 3n + 1
+class Collatz
+  
+  def collatz(num)
+    sequence = []
+    sequence << num
+    puts sequence
+    return sequence if num == 1
 
-def collatz(n)
-  arr = []
-  arr << n
-  p arr
-  return arr if n <= 1
+    return collatz(num/2) if num % 2 == 0
+    return collatz(3*num + 1)
+  end
 
-  return collatz(n/2) if n % 2 == 0
-  return collatz(3*n + 1)
+  def longest(range)
+    longest = []
+
+    1.upto(range) do |i|
+      current = []
+      current << collatz(i)
+      longest = current if current.size > longest.size
+    end
+
+    puts longest[0]
+  end
+
 end
 
-collatz(7)
+first = Collatz.new
+first.collatz(5)
+
+
+# consider making it class
+# finding which sequence is longest, only hold the
+# length of the previous sequence and replace if the 
+# length of current is greater
