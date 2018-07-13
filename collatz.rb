89d9dd46e6586f2e4e2ef03f96af7b91f522740d
mb-kaizen <michael.entr@gmail.com>
2018-07-13 00:51:18 +0000

    finished collatz!

--- collatz.rb
+++ collatz.rb
@@ -2,11 +2,6 @@
 # n is even: n -> n/2
 # n is odd: n -> 3n + 1
 class Collatz
-  attr_accessor :sequence
-
-  def initialize
-    @sequence = []
-  end
 
   def collatz(num, output=[])
     if num == 1
@@ -15,31 +10,20 @@ class Collatz
     end 
 
     return collatz(num/2, output << num) if num % 2 == 0
-    return collatz(3*num + 1, output << num)
+    collatz(3*num + 1, output << num)
   end
 
   def longest(range)
     longest = []
     1.upto(range) do |i|
-      longest << collatz(i) if collatz(i).size > longest.size
-      p longest
+      current = []
+      current << collatz(i) 
+      longest = current[0] if longest.size < current[0].size 
     end
-    p "-----"
-    p longest
-    return longest
-  end
-
-  def show
-    p sequence
+    p "#{longest[0]} produced the longest chain of collatz values with a total length of #{longest.size}!"
   end
 
 end
 
 first = Collatz.new
-first.longest(5)
-
-
-# consider making it class
-# finding which sequence is longest, only hold the
-# length of the previous sequence and replace if the 
-# length of current is greater
+first.longest(100)
