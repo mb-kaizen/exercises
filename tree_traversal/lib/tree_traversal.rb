bf73a291764033cd45c99aaa4fe018df6ad27f31
mb-kaizen <michael.entr@gmail.com>
2018-07-30 05:32:35 +0000

    completed both depth and breadth first search for tree traversal

--- tree_traversal/lib/tree_traversal.rb
+++ tree_traversal/lib/tree_traversal.rb
@@ -6,12 +6,6 @@ class Tree
     @children = children
   end
 
-# just moved the functions below into the module
-
-  def show_stuff(trunk_node)
-    p trunk_node.children
-  end
-
   # perform pre-order operation
   # for any tree do above and below:
   # for each i from 1 to the number of children do:
@@ -26,7 +20,18 @@ class Tree
     depth_first_search(tree.children[1], 11)
   end
 
-  def breadth_first_search()
+  def breadth_first_search(tree, desired_payload)
+    queue = []
+    queue << tree
+
+    while queue.size != 0
+      node = queue.shift
+      p node.payload
+      return node if node.payload == desired_payload
+      queue << node.children[0] if node.children[0] != nil
+      queue << node.children[1] if node.children[1] != nil
+    end
+  end
 
 end
 
@@ -44,4 +49,4 @@ shallow_fifth_node = Tree.new(5, [ninth_node])
 # The "trunk" of the tree
 trunk_node = Tree.new(2, [seventh_node, shallow_fifth_node])
 
-trunk_node.depth_first_search(trunk_node, 11)
+trunk_node.breadth_first_search(trunk_node, 11)
