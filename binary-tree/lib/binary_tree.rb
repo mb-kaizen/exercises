054feeb9543bfa96171425c21c9aea71fd44b1ba
mb-kaizen <michael.entr@gmail.com>
2018-08-12 03:56:12 +0000

    finished binary tree sort

--- binary-tree/lib/binary_tree.rb
+++ binary-tree/lib/binary_tree.rb
@@ -1,18 +1,53 @@
-class BinaryTree
-  attr_accessor :payload, :left, :right
+class BinarySearchTree
+  class Node
+    attr_accessor :value, :left, :right
 
-  def initialize(payload, left, right)
-    @payload = payload
-    @left = left
-    @right = right
+    # Creates node
+    def initialize(value)
+      @value = value
+      @left = nil
+      @right = nil
+    end
+
+    def insert_node(new_value)
+      if new_value <= @value
+        @left.nil? ? @left = Node.new(new_value) : @left.insert_node(new_value)
+      elsif new_value > @value
+        @right.nil? ? @right = Node.new(new_value) : @right.insert_node(new_value)    
+      end
+    end
   end
 
+  attr_reader :root
+  def initialize
+    @root = nil
+  end
+
+  def insert(value)
+    if @root.nil?
+      @root = Node.new(value)
+    else
+      @root.insert_node(value)
+    end
+  end
+
+  def build_tree(array)
+    array.each do |element|
+      self.insert(element)
+    end
+    self
+  end
+
+  # In_order_sort (Left, Root, Right)
+  def sort(node)
+    return if node == nil
+
+    sort(node.left)
+    p node.value
+    sort(node.right)
+  end
 end
 
-seven = BinaryTree.new(7, nil, nil)
-five  = BinaryTree.new(5, nil, nil)
-four  = BinaryTree.new(4, nil, nil)
-six   = BinaryTree.new(6, nil, seven)
-three = BinaryTree.new(3, nil, six)
-two   = BinaryTree.new(2, four, five)
-trunk = BinaryTree.new(1, two, three)
+tree = BinarySearchTree.new
+tree.build_tree([7, 4, 9, 1, 6, 14, 10])
+tree.sort(tree.root)
